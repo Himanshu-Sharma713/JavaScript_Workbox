fdf590ce5d1a27d8ea95d81947d4b087426000e4
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-11 13:23:11 +0530

    write first code in JavaScript

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,3 +1,8 @@
 // Learn JavaScript
 
 console.log("Hello, World!");
+
+var fullName = "Himanshu Sharma";
+var age = 22;
+
+console.log("Hello, My Name is: ", fullName, " & my age is: ", age, "!....");
