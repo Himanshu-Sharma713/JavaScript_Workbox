ce084467aaa35d838d79cafa1177663f0142fe44
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-12 13:27:11 +0530

    day-1 completed

--- Day-1/script2.js
+++ Day-1/script2.js
@@ -7,8 +7,29 @@
 // let b;
 
 
-b = 49;
+//b = 49;
 
-console.log(b);
+// console.log(b);
 
-var b;
+// var b;
+
+
+// ====================== 
+
+let a = 10;
+let b = 5;
+
+let add = a + b;
+console.log(add);
+
+let sub = a - b;
+console.log(sub);
+
+let multi = a * b;
+console.log(multi);
+
+let div = a / b;
+console.log(div);
+
+let mod = a % b;
+console.log(mod);
