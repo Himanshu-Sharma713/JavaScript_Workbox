8b319b430c61181ae2c0f167e83c537c21e95676
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-12 13:06:38 +0530

    ok Variables

--- Day-1/script1.js
+++ Day-1/script1.js
@@ -1,10 +1,13 @@
 // var, let & const
 
 var a = 5;
+var a = 25;
 console.log(a);
 
 let b = 7;
+// let b = 49; ==>> Uncaught SyntaxError: Identifier 'b' has already been declared
 console.log(b);
 
 const c = 11;
+// c = 55; ==>>   Uncaught TypeError: Assignment to constant variable.
 console.log(c);
