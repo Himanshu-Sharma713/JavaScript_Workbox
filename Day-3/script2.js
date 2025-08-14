ce2955fa85a6c89bb0296aea32453ead4ffecda1
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-14 13:06:52 +0530

    not true or not false

--- Day-3/script2.js
+++ Day-3/script2.js
@@ -9,3 +9,20 @@ console.log(++b, b);
 
 console.log(c--, c);
 console.log(--d, d);
+
+
+let A = 10;
+let B = 40;
+
+let res1 = A == B && A <= B;
+
+console.log(res1);
+
+let res2 = A == B || A <= B;
+
+console.log(res2);
+
+
+console.log(!true);
+console.log(!false);
+
