895d1ae57fa752777424e1cb16adbaf82388b2bb
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-14 12:45:12 +0530

    gearter than & less than & equals to

---
+++ Day-3/script1.js
@@ -0,0 +1,19 @@
+let a = 7;
+let b = 49;
+let c = "7";
+
+console.log(">,");
+
+
+console.log(a > b);
+console.log(a < b);
+
+console.log(a <= b);
+console.log(a >= b);
+
+console.log(a > c);
+console.log(a < c);
+
+console.log(a <= c);
+console.log(a >= c);
+
