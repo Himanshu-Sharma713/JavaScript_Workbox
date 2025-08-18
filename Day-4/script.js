ca091596e42598ad603a9b3622bdd4109a8c140b
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-18 12:21:12 +0530

    condition operator

---
+++ Day-4/script.js
@@ -0,0 +1,11 @@
+// let condition = false;
+// let fullName = "Himanshu Sharma"
+// let greet = "Hello, World!"
+
+// condition ? console.log(fullName) : console.log(greet);
+
+let a = 5, b = 10;
+
+let res = a <= b ? "Correct" : "Incorrect";
+
+console.log(res);
