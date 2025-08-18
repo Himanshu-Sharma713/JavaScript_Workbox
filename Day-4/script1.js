c950235b62a8967784301ccd6c088c80b0b1030b
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-18 12:43:25 +0530

    if-else part-2

---
+++ Day-4/script1.js
@@ -0,0 +1,15 @@
+let x = 5;
+
+// if (x > 10) {
+//   console.log("X is  greater than 10");
+// } else {
+//   console.log("X is less than or equal 10");
+// }
+
+if (x > 10) {
+  console.log("Correct");
+} else if (x == 10) {
+  console.log("Equal to 10");
+} else {
+  console.log("Less than 10");
+}
