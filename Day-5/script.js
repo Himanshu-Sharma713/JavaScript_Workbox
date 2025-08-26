a91d3cb7dae7a16ef000587906fb90a79030b3ea
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-26 15:01:19 +0530

    shift and unshift array method in JS

--- Day-5/script.js
+++ Day-5/script.js
@@ -3,5 +3,13 @@ let arr = [1, 2, 3, 4, 5];
 // let res = arr.includes(3);
 // console.log(res);
 
-let res = arr.reverse();
-console.log(res);
+// let res = arr.reverse();
+// console.log(res);
+
+arr.unshift(0);
+
+console.log(arr);
+
+arr.shift();
+
+console.log(arr);
