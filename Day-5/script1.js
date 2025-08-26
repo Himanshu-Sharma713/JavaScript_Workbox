8a36997527710da8686bc915a649821e05808aa1
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-26 15:22:46 +0530

    create all types of function  in JavaScript.

--- Day-5/script1.js
+++ Day-5/script1.js
@@ -1,1 +1,24 @@
 // functions
+let name = "Himanshu Sharma";
+let age = 22;
+function sayHello() {
+  console.log(`Hello, My Name is: ${name}, My Age is: ${age}....`);
+}
+
+sayHello();
+
+const greeting = (fullName, Age) => {
+  console.log(`Hello, My Name is: ${fullName}, My Age is: ${Age}....`);
+};
+
+greeting("Deepak Kumar", 23);
+
+const functionExpression = function (a, b) {
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(a / b);
+};
+
+functionExpression(5, 4);
+            
