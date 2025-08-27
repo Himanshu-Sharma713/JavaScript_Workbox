225f8b678a029a04b4e512759c0ce4dea4332453
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-27 16:00:31 +0530

    while in JS

--- Day-6/script1.js
+++ Day-6/script1.js
@@ -1,7 +1,17 @@
 // loops part-2
 
-let num = prompt("Enter Number: ");
+// let num = prompt("Enter Number: ");
 
-for (let i = 1; i <= 10; i++) {
-  console.log(num + " X " + i + " = " + num * i);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${num} X ${i} = ${num * i}`);
+// }
+
+// loops
+
+console.log("=========== While Loop =========");
+
+let i = 1;
+while (i <= 10) {
+  console.log("This Number is: ", i);
+  i++;
 }
