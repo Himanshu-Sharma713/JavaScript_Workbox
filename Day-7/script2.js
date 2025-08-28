c7cd100fd5d4e879e18f79245eb025d6547c8f56
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-28 15:48:00 +0530

    Question in String using JS

---
+++ Day-7/script2.js
@@ -0,0 +1,19 @@
+// String
+
+let str = "Himanshu Sharma";
+str = str.toLowerCase();
+let num = 0;
+
+for (let i = 0; i < str.length; i++) {
+  if (
+    str.charAt(i) === "a" ||
+    str.charAt(i) === "e" ||
+    str.charAt(i) === "i" ||
+    str.charAt(i) === "o" ||
+    str.charAt(i) === "u"
+  ) {
+    num++;
+  }
+}
+
+console.log(num);
