d9d9048e39652287f0e4e367886aec4ed69a3c69
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-28 15:33:02 +0530

    Smallest Number in Array in JavaScript

--- Day-7/script1.js
+++ Day-7/script1.js
@@ -9,3 +9,14 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 console.log("Largest Number is: ", largestNumber);
+
+// Find the Smallest number in array
+
+let smallestNumber = Number.MAX_VALUE;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < smallestNumber) {
+    smallestNumber = arr[i];
+  }
+}
+
+console.log("Smallest Number is: ", smallestNumber);
