b6ea53e4ecc053808db5dbd64524608228c557a8
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-28 15:30:57 +0530

    Largest Number in Array using JavaScript

---
+++ Day-7/script1.js
@@ -0,0 +1,11 @@
+// Find the largest number in array
+
+let arr = [4, 33, 1, 2, 7, 88, 91, 15];
+let largestNumber = Number.MIN_VALUE;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > largestNumber) {
+    largestNumber = arr[i];
+  }
+}
+
+console.log("Largest Number is: ", largestNumber);
