e843091e1327ca3db95933061269335514a99829
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-08-28 15:19:48 +0530

    Question no-2 in JS

--- Day-7/script.js
+++ Day-7/script.js
@@ -9,18 +9,33 @@
 //   i++;
 // } while (i < 10);
 
-// ==============>>>>>>>>>>>>>>-------     Question 1
+// ==============>>>>>>>>>>>>>>-------   Question 1
 
-let arr = [4, 8, 2, 0, 5, 7, 1, 1, 0];
-let even = 0;
-let odd = 0;
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 === 0) {
-    even++;
+// let arr = [4, 8, 2, 0, 5, 7, 1, 1, 0];
+// let even = 0;
+// let odd = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     even++;
+//   } else {
+//     odd++;
+//   }
+// }
+
+// console.log("Even element: ", even);
+// console.log("Odd element: ", odd);
+
+let arr = [0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1];
+
+let one = 0;
+let zero = 0;
+for (const ele of arr) {
+  if (ele === 1) {
+    one++;
   } else {
-    odd++;
+    zero++;
   }
 }
 
-console.log("Even element: ", even);
-console.log("Odd element: ", odd);
+console.log("Number of One: ", one);
+console.log("Number of Zero: ", zero);
