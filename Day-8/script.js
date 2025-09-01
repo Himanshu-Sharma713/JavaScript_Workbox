5051aaf437aa590e56ec5909039f498d5188ba29
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-01 15:23:03 +0530

    using loop in object in Javascript

--- Day-8/script.js
+++ Day-8/script.js
@@ -4,10 +4,9 @@ let obj = {
   fullName: "Himanshu Sharma",
   Age: 22,
   Course: "Full Stack Java Development",
-  Hobbies: ["Reading", "Travelling", "Listening Musics"],
+  Hobbies: ["Reading", " Travelling", " Listening Musics "],
 };
 
-console.log(obj.fullName);
-console.log(obj.Age);
-console.log(obj.Course);
-console.log(obj.Hobbies);
+for (const key in obj) {
+  console.log(`${key} = ${obj[key]}`);
+}
