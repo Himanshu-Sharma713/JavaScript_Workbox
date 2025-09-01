e239da915c4233abfddcce97c75cf21a62258666
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-01 15:36:49 +0530

    Object create using Function

--- Day-8/script.js
+++ Day-8/script.js
@@ -1,12 +1,20 @@
 // Javascript Object
 
-let obj = {
-  fullName: "Himanshu Sharma",
-  Age: 22,
-  Course: "Full Stack Java Development",
-  Hobbies: ["Reading", " Travelling", " Listening Musics "],
-};
+// let obj = {
+//   fullName: "Himanshu Sharma",
+//   Age: 22,
+//   Course: "Full Stack Java Development",
+//   Hobbies: ["Reading", " Travelling", " Listening Musics "],
+// };
 
-for (const key in obj) {
-  console.log(`${key} = ${obj[key]}`);
+// for (const key in obj) {
+//   console.log(`${key} = ${obj[key]}`);
+// }
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
 }
+
+const car1 = new Car("Eagle", "Talon TSi", 1993);
