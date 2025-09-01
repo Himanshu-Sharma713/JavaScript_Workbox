ad4503c799ef032595c733c3cf99099a82141a18
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-01 15:07:18 +0530

    learn and Create an Object in Javascript

---
+++ Day-8/script.js
@@ -0,0 +1,13 @@
+// Javascript Object
+
+let obj = {
+  fullName: "Himanshu Sharma",
+  Age: 22,
+  Course: "Full Stack Java Development",
+  Hobbies: ["Reading", "Travelling", "Listening Musics"],
+};
+
+console.log(obj.fullName);
+console.log(obj.Age);
+console.log(obj.Course);
+console.log(obj.Hobbies);
