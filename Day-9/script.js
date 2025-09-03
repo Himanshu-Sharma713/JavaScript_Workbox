ea26a8104deb0e93f99017c001bd7b25c1bf9ca1
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-03 15:26:06 +0530

    Set Method

--- Day-9/script.js
+++ Day-9/script.js
@@ -1,30 +1,32 @@
-// map and set
+// // map and set
 
-let map = new Map();
-map.set("fullName", "Himanshu Sharma");
-map.set("Age", 22);
-map.set("Email", "himanshusb713@gmail.com");
-map.set("Hobbies", ["Reading", "Listening Music", "Travelling"]);
-map.set("isTrue", true);
+// let map = new Map();
+// map.set("fullName", "Himanshu Sharma");
+// map.set("Age", 22);
+// map.set("Email", "himanshusb713@gmail.com");
+// map.set("Hobbies", ["Reading", " Listening Music", " Travelling"]);
+// map.set("isTrue", true);
 
-console.log(map);
-// console.log(map.has("agel"));
-// console.log(map.has("Age"));
-
-// console.log(map.get("fullName"));
-// console.log(map.get("FullName"));
-
-// console.log(map.size);
-
-// // map.clear();
-
-// map.delete("isTrue");
-// console.log(map.size);
 // console.log(map);
+// // console.log(map.has("agel"));
+// // console.log(map.has("Age"));
 
-// console.log(map.keys());
-// console.log(map.values());
+// // console.log(map.get("fullName"));
+// // console.log(map.get("FullName"));
 
-for (let [key, value] of map) {
-  console.log(`${key} => ${value}`);
-}
+// // console.log(map.size);
+
+// // // map.clear();
+
+// // map.delete("isTrue");
+// // console.log(map.size);
+// // console.log(map);
+
+// // console.log(map.keys());
+// // console.log(map.values());
+
+// for (let [key, value] of map) {
+//   console.log(`${key} => ${value}`);
+// }
+
+let set = new Set();
