1f8b8a16eaa668e350a48360c2550ba0524ee678
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-03 14:49:49 +0530

    Create an Object using Map and set method.

---
+++ Day-9/script.js
@@ -0,0 +1,10 @@
+// map and set
+
+let map = new Map();
+map.set("fullName", "Himanshu Sharma");
+map.set("Age", 22);
+map.set("Email", "himanshusb713@gmail.com");
+map.set("Hobbies", ["Reading", "Listening Music", "Travelling"]);
+map.set("isTrue", true);
+
+console.log(map);
