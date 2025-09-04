bdefe22a020f7d58d5f11e714cbc7aa88df868d8
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-04 15:07:51 +0530

    2d Array complted

--- Day-10/script.js
+++ Day-10/script.js
@@ -6,4 +6,19 @@ let arr = [
   [7, 8, 9],
 ];
 
+// console.log(arr[2][2]); // row = 2, column = 2
+// console.log(arr[2][6]); // undefined
+
+// let arr1 = [];
+// let row = 3;
+// let col = 3;
+// let val = 1;
+
+// for (let i = 0; i < row; i++) {
+//   arr1[i] = [];
+//   for (let j = 0; j < col; j++) {
+//     arr1[i][j] = val++;
+//   }
+// }
+
 console.log(arr);
