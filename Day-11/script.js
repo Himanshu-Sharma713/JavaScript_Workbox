63fd849fd3db6655afacad4163864de2b370f5e8
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-09 15:16:47 +0530

    access by get class name

--- Day-11/script.js
+++ Day-11/script.js
@@ -1,6 +1,12 @@
 // DOM  => Document Object Model
 
-function changeButton() {
+function changeHeading() {
   const ele = document.getElementsByTagName("h2");
-  ele[0].innerText="Hello, JavaScript!";
+  ele[0].innerText = "Hello, JavaScript!";
+}
+
+function changePara() {
+  const ele = document.getElementsByClassName("para");
+  ele[0].innerText =
+    "Attitude is a choice. Happiness is a choice. Optimism is a choice. Kindness is a choice. Giving is a choice. Respect is a choice. Whatever choice you make makes you. Choose wisely.";
 }
