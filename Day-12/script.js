45b029a1abdbcce35ae5a4029a55b2de3cdaf0ab
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-10 14:59:11 +0530

    clock completed

--- Day-12/script.js
+++ Day-12/script.js
@@ -1,3 +1,25 @@
 function change() {
   const ele = document.getElementById("clock");
+  let date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let time = `${hour} : ${min} : ${seconds}`;
+
+  ele.innerHTML = time;
+  setTimeout(() => {
+    change();
+  }, 1000);
 }
+change();
+
+setTimeout(() => {
+  console.log("1");
+  setTimeout(() => {
+    console.log("2");
+    setTimeout(() => {
+      console.log("3");
+    }, 3000);
+  }, 2000);
+}, 1000);
