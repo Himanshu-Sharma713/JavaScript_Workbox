33016e40b4841bd7d2daa934dae705eea869c51d
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-15 15:43:34 +0530

    do nothing, only reading

---
+++ Day-13/script.js
@@ -0,0 +1,19 @@
+document.writeln("===============================");
+document.writeln("<br/>");
+document.writeln("JSON");
+document.writeln("<br/>");
+document.writeln("===============================");
+document.writeln("<br/>");
+document.writeln("API's");
+document.writeln("<br/>");
+document.writeln("===============================");
+document.writeln("<br/>");
+document.writeln("Async Await");
+document.writeln("<br/>");
+document.writeln("===============================");
+document.writeln("<br/>");
+document.writeln("Promises");
+document.writeln("<br/>");
+document.writeln("===============================");
+
+
