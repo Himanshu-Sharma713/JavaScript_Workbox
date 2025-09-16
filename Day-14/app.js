ddeeaf185a7b9559bcb7551742eb190d0864f8c5
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-16 14:58:04 +0530

    setup API's

--- Day-14/app.js
+++ Day-14/app.js
@@ -1,7 +1,9 @@
 // API
 
 async function fetchData() {
-  const data = await fetch();
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
   let res = data.json();
   console.log(res);
 }
+
+fetchData();
