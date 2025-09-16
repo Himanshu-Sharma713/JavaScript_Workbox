67953edf036381f67f685e967798eed31be52ff2
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-16 15:15:30 +0530

    next part of API

--- Day-14/app.js
+++ Day-14/app.js
@@ -1,9 +1,23 @@
 // API
 
 async function fetchData() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/users");
-  let res = data.json();
-  console.log(res);
+  const data = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => {
+      if (!response.ok) {
+        console.log("Error");
+      }
+      return response.json();
+    }
+  );
+
+  const userData = data.map((user) => {
+    return {
+      name: user.name,
+      email: user.email,
+    };
+  });
+
+  console.log(userData);
 }
 
 fetchData();
