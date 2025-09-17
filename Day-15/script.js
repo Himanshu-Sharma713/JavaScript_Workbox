61d62e304b8a47b95743a59d3eaaad43f01200a2
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-17 14:52:11 +0530

    used api

---
+++ Day-15/script.js
@@ -0,0 +1,20 @@
+// API
+
+let API = "./data.json";
+
+async function fetchData() {
+  const data = await fetch(API)
+    .then((response) => {
+      if (!response.ok) {
+        console.log("Error");
+      }
+      return response.json();
+    })
+    .then((userData) => {
+      const matureUsers = userData.filter((user) => {
+        return user.age <= 30;
+      });
+      console.log(matureUsers);
+    });
+}
+fetchData();
