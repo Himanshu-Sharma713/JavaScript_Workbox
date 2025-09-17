aeae0b16fc3e52de4c25586363dd39d1ab79a6df
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-17 15:24:33 +0530

    Image API

--- Day-15/script.js
+++ Day-15/script.js
@@ -16,5 +16,17 @@ async function fetchData() {
       });
       console.log(matureUsers);
     });
+  let maturelistDiv = document.getElementById("matureList");
+  if (maturelistDiv) {
+    if (matureUsers.length === 0) {
+      maturelistDiv.textContent = "No mature users list found";
+    } else {
+      matureUsers.forEach((user) => {
+        const p = document.createElement("p");
+        p.textContent = `${user.name} ${user.age} ${user.contact}`;
+        maturelistDiv.appendChild(p);
+      });
+    }
+  }
 }
 fetchData();
