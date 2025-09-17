81704a35d3dc1f15d541544380d7ec0c83697ffb
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-17 15:33:28 +0530

    start working API

--- Day-15/script.js
+++ Day-15/script.js
@@ -14,19 +14,18 @@ async function fetchData() {
       const matureUsers = userData.filter((user) => {
         return user.age <= 30;
       });
-      console.log(matureUsers);
+      let maturelistDiv = document.getElementById("maturelist");
+      if (maturelistDiv) {
+        if (matureUsers.length === 0) {
+          maturelistDiv.textContent = "No mature users list found";
+        } else {
+          matureUsers.forEach((user) => {
+            const p = document.createElement("p");
+            p.textContent = `${user.name} ${user.age} ${user.contact}`;
+            maturelistDiv.appendChild(p);
+          });
+        }
+      }
     });
-  let maturelistDiv = document.getElementById("matureList");
-  if (maturelistDiv) {
-    if (matureUsers.length === 0) {
-      maturelistDiv.textContent = "No mature users list found";
-    } else {
-      matureUsers.forEach((user) => {
-        const p = document.createElement("p");
-        p.textContent = `${user.name} ${user.age} ${user.contact}`;
-        maturelistDiv.appendChild(p);
-      });
-    }
-  }
 }
 fetchData();
