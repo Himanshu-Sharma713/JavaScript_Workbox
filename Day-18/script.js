af23281bea4ac16e06d55bddc16380614cd0ab9f
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-25 14:52:03 +0530

    second example

--- Day-18/script.js
+++ Day-18/script.js
@@ -1,5 +1,14 @@
+// first Class Function in Javascript... example-1
 const foo = function () {
   console.log("Hello, World!");
 };
 
-foo();
+// foo();
+
+// first Class Function in Javascript... example-2
+
+let greet = function (val) {
+  val();
+};
+
+greet(foo);
