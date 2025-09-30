3015f0e8b326b328f05aa4aec20cd7dd6edef3cc
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-09-30 15:11:45 +0530

    add Style using DOM

--- Day-19/script.js
+++ Day-19/script.js
@@ -4,4 +4,8 @@ function change() {
   let ele = document.getElementsByClassName("head");
   ele[0].innerText = `Count = ${i}`;
   i++;
+
+  let ele1 = document.getElementById("heading");
+  ele1.style.backgroundColor = "pink";
+  ele1.style.color = "blue";
 }
