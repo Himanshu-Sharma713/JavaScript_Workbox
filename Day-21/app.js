b4b6eeaf6f10474f4ef6d3426197d9b7c48a1442
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-10-06 15:48:15 +0530

    callback funtion

--- Day-21/app.js
+++ Day-21/app.js
@@ -4,4 +4,19 @@ async function fetchData() {
   console.log(res);
 }
 
-fetchData();
+// fetchData();
+
+console.log(
+  "========================================================================="
+);
+
+function greet(name, callback) {
+  console.log("Hello, " + name);
+  callback();
+}
+
+function sayBye() {
+  console.log("Goodbye!");
+}
+
+greet("Ajay", sayBye);
