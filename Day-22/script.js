e281c97dc70ddabf3ffc11cf020beb0722eb2993
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-10-07 15:08:13 +0530

    map method in js

--- Day-22/script.js
+++ Day-22/script.js
@@ -1,14 +1,20 @@
-let arr = [1, 2, 3, "HTML", 4, 5, 6, "CSS", 7, 8, 9, "JS"];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(arr);
 
-let a = arr.shift();
+// let a = arr.shift();
 
-console.log(a);
-console.log(arr);
+// console.log(a);
+// console.log(arr);
 
-let b = arr.unshift(a);
+// let b = arr.unshift(a);
 
-console.log(b);
-console.log(arr);
+// console.log(b);
+// console.log(arr);
 
 console.log("============================================================");
+
+let brr = arr.map((e) => {
+  return e ** 2;
+});
+
+console.log(brr);
