e38a2bb9e62071c2bad5de031f44c5865613b19a
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-10-07 15:04:00 +0530

    Array Methods Shift or UnShift

---
+++ Day-22/script.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, "HTML", 4, 5, 6, "CSS", 7, 8, 9, "JS"];
+console.log(arr);
+
+let a = arr.shift();
+
+console.log(a);
+console.log(arr);
+
+let b = arr.unshift(a);
+
+console.log(b);
+console.log(arr);
+
+console.log("============================================================");
