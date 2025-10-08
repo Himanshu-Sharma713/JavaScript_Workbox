93bc28994e15b6c110ccead5a2a87d8666d32cdf
Himanshu-Sharma-713 <himanshusb713@gmail.com>
2025-10-08 14:58:01 +0530

    for each and for in loops in JS

---
+++ Day-23/script.js
@@ -0,0 +1,18 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+arr.forEach((e) => console.log(e));
+
+console.log("=======================================================");
+
+let student = {
+  name: "Himanshu Sharma",
+  age: 22,
+  email: "himanshusb713@gmail.com",
+  contact: 70782122145,
+};
+
+for (const key in student) {
+  console.log(`${key} : ${student[key]}`);
+}
+
+console.log("=======================================================");
